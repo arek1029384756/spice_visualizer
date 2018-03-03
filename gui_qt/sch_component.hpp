114dadbe87a7072e59df99785b7b6ebfde744182
Arek <arek@arek>
2018-03-03 15:39:09 +0100

    Added NPN transistor schematic symbol

--- gui_qt/sch_component.hpp
+++ gui_qt/sch_component.hpp
@@ -14,11 +14,11 @@ namespace gui_qt {
         qreal m_margin;
 
         static QPen getBodyPen() {
-            return QPen(Qt::black, 4, Qt::SolidLine, Qt::RoundCap, Qt::RoundJoin);
+            return QPen(Qt::black, gridRaster * 0.4, Qt::SolidLine, Qt::RoundCap, Qt::RoundJoin);
         }
 
         static QPen getTerminalPen() {
-            return QPen(Qt::black, 2, Qt::SolidLine, Qt::RoundCap, Qt::RoundJoin);
+            return QPen(Qt::black, gridRaster * 0.2, Qt::SolidLine, Qt::RoundCap, Qt::RoundJoin);
         }
 
         protected:
@@ -95,6 +95,43 @@ namespace gui_qt {
 
         public:
         Capacitor()
+            : SchComponent(length, width, margin) {
+        }
+
+    };
+
+    class NpnTransistor : public SchComponent {
+
+        static constexpr qreal length = gridRaster * 8;
+        static constexpr qreal width = gridRaster * 8;
+        static constexpr qreal margin = gridRaster;
+
+        protected:
+        void drawBody(QPainter* painter) const override {
+            painter->drawLine(length / 2 - margin, margin * 2.5, length / 2 - margin, width - margin * 2.5);
+            painter->drawEllipse(QPointF(length / 2, width / 2), length / 2 - margin, width / 2 - margin);
+
+            painter->setPen(getConnectionPen());
+            painter->drawLine(length / 2 - margin, width / 2, length - margin * 3, margin * 2);
+            painter->drawLine(length / 2 - margin, width / 2, length - margin * 3, width - margin * 2);
+
+            static const QPointF arrowPoints[] = { QPointF(length - margin * 3, width - margin * 2),
+                                                   QPointF(length - margin * 3.75, width - margin * 3.25),
+                                                   QPointF(length - margin * 4.25, width - margin * 2.75),
+                                                };
+            painter->setPen(getConnectionPen());
+            painter->setBrush(QBrush(Qt::black, Qt::SolidPattern));
+            painter->drawPolygon(arrowPoints, 3);
+        }
+
+        void drawTerminals(QPainter* painter) const override {
+            painter->drawLine(0, width / 2, length / 2 - margin, width / 2);
+            painter->drawLine(length - margin * 3, width, length - margin * 3, width - margin * 2);
+            painter->drawLine(length - margin * 3, 0, length - margin * 3, margin * 2);
+        }
+
+        public:
+        NpnTransistor()
             : SchComponent(length, width, margin) {
         }
 
--- gui_qt/schematic.cpp
+++ gui_qt/schematic.cpp
@@ -18,7 +18,7 @@ namespace gui_qt {
 
 
         //Temporary test
-        m_view->fitInView(200, 50, 500, 150, Qt::KeepAspectRatio);
+        m_view->fitInView(200, 50, 700, 150, Qt::KeepAspectRatio);
 
         SchComponent* res = new Resistor();
         res->setX(300);
@@ -26,16 +26,28 @@ namespace gui_qt {
         m_scene->addItem(res);
 
         SchComponent* cap = new Capacitor();
-        cap->setX(400);
+        cap->setX(420);
         cap->setY(100);
         m_scene->addItem(cap);
 
+        SchComponent* tran = new NpnTransistor();
+        tran->setX(500);
+        tran->setY(80);
+        m_scene->addItem(tran);
+
         auto resRect = res->boundingRect();
         auto capRect = cap->boundingRect();
+        auto tranRect = tran->boundingRect();
         m_scene->addLine(res->x() + resRect.width(),
                          res->y() + resRect.height() / 2,
                          cap->x(),
                          cap->y() + capRect.height() / 2,
+                         SchComponent::getConnectionPen());
+
+        m_scene->addLine(cap->x() + capRect.width(),
+                         cap->y() + capRect.height() / 2,
+                         tran->x(),
+                         tran->y() + tranRect.height() / 2,
                          SchComponent::getConnectionPen());
         //
 
